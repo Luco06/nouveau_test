643fafda514474a868d55c35f069a5ac6d16a923
Luco06 <luc-olivier.yohan@hotmail.fr>
2020-11-23 13:23:22 +0100

    commit_instance

---
+++ instance.py
@@ -0,0 +1,18 @@
+from etudiant import pokemon
+
+
+pikachu = pokemon("Pikachu", 0.6, "Jaune", "Raichu", "pikapika", "Foudre", "Eclair" )
+print(pikachu.nom)
+print(pikachu.rugisement())
+pikachu.setter_nom(" Lugia")
+print(pikachu.nom)
+#print(pikachu.pokemon_attaque1())
+#print(pikachu.metamorphose())
+#print(pikachu.nom)
+#print(pikachu.pokemon_attaque2())
+    
+#carapuce = pokemon("Carapuce", 0.4, "Bleu", "Carabafe", "blorp" ) 
+#print(carapuce.nom)
+#print(carapuce.evolution)
+#print(carapuce.rug)
+#print(carapuce.rugisement())
